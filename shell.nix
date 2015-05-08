da48fb29a913ec589ceeac133baaf7dc79e1c9dd
Thomas Tuegel <ttuegel@gmail.com>
2015-05-08 07:22:11 -0500

    Remove unused dependencies

--- shell.nix
+++ shell.nix
@@ -1,12 +1,3 @@
 with (import <nixpkgs> {}).pkgs;
-with haskellngPackages.override {
-  overrides = self: super: rec {
-    blaze-builder = self.callPackage ./blaze-builder.nix {};
-    http-streams = haskell-ng.lib.overrideCabal (self.callPackage ./http-streams.nix {
-      http-common = self.callPackage ./http-common.nix {};
-    }) (drv: drv // { doCheck = false; });
-  };
-};
-let
-in
-  (callPackage ./. {}).env
+with haskellngPackages;
+(callPackage ./. {}).env
