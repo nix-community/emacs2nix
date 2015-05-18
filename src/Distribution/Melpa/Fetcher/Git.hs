3e5e12207c5ee29bd5b3b9dc2f87bf8ebad3415d
Thomas Tuegel <ttuegel@gmail.com>
2015-05-18 14:25:17 -0500

    Implement GitHub fetcher

--- src/Distribution/Melpa/Fetcher/Git.hs
+++ src/Distribution/Melpa/Fetcher/Git.hs
@@ -2,7 +2,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
 
-module Distribution.Melpa.Fetcher.Git ( Git, fetchGit ) where
+module Distribution.Melpa.Fetcher.Git ( Git, fetchGit, getRev_Git ) where
 
 import Control.Error hiding (runScript)
 import Control.Exception (bracket)
@@ -33,17 +33,20 @@ instance FromJSON Git where
 fetchGit :: Fetcher Git
 fetchGit = Fetcher {..}
   where
-    getRev name Git {..} tmp =
-      let args = [ "log", "--first-parent", "-n1", "--pretty=format:%H" ]
-                 ++ maybeToList (T.unpack <$> branch)
-      in EitherT $ bracket
-        (S.runInteractiveProcess "git" args (Just tmp) Nothing)
-        (\(_, _, _, pid) -> S.waitForProcess pid)
-        (\(inp, out, _, _) -> do
-               S.write Nothing inp
-               revs <- S.lines out >>= S.decodeUtf8 >>= S.toList
-               return $ headErr (name <> ": could not find revision") revs)
-
+    getRev name Git {..} tmp = getRev_Git name branch tmp
     prefetch name Git {..} rev =
       let args = [ "--url", T.unpack url, "--rev", T.unpack rev ]
       in prefetchWith name "nix-prefetch-git" args
+
+getRev_Git :: Text -> Maybe Text -> FilePath -> EitherT Text IO Text
+getRev_Git name branch tmp =
+  EitherT $ bracket
+    (S.runInteractiveProcess "git" gitArgs (Just tmp) Nothing)
+    (\(_, _, _, pid) -> S.waitForProcess pid)
+    (\(inp, out, _, _) -> do
+           S.write Nothing inp
+           revs <- S.lines out >>= S.decodeUtf8 >>= S.toList
+           return $ headErr (name <> ": could not find revision") revs)
+  where
+    gitArgs = [ "log", "--first-parent", "-n1", "--pretty=format:%H" ]
+              ++ maybeToList (T.unpack <$> branch)
--- src/Distribution/Melpa/Fetcher/GitHub.hs
+++ src/Distribution/Melpa/Fetcher/GitHub.hs
@@ -4,15 +4,15 @@
 
 module Distribution.Melpa.Fetcher.GitHub ( GitHub, fetchGitHub ) where
 
-import Control.Error
 import Data.Aeson
 import Data.Aeson.Types (defaultOptions)
-import Data.HashMap.Strict (HashMap)
-import qualified Data.HashMap.Strict as HM
+import Data.Monoid ((<>))
 import Data.Text (Text)
+import qualified Data.Text as T
 import GHC.Generics
 
 import Distribution.Melpa.Fetcher
+import Distribution.Melpa.Fetcher.Git (getRev_Git)
 
 data GitHub =
   GitHub
@@ -29,27 +29,12 @@ instance FromJSON GitHub where
   parseJSON = genericParseJSON defaultOptions
 
 fetchGitHub :: Fetcher GitHub
-fetchGitHub = undefined
-
-{-
-hash :: FilePath -> FilePath -> Bool -> Text -> Archive -> Recipe
-     -> EitherT Text IO Package
-hash melpa nixpkgs stable name arch rcp = do
-  let GitHub _github@(Fetcher {..}) = fetcher rcp
-  _commit <- getCommit melpa stable name Nothing (githubEnv name _github)
-  _github <- return _github { commit = Just _commit }
-  _hash <- prefetch nixpkgs name Nothing (githubEnv name _github)
-  return Package
-    { Package.ver = Archive.ver arch
-    , Package.deps = maybe [] HM.keys (Archive.deps arch)
-    , Package.recipe = rcp { fetcher = GitHub _github }
-    , Package.hash = _hash
-    }
-
-githubEnv :: Text -> GitHub -> HashMap Text Text
-githubEnv name Fetcher {..} =
-  HM.fromList
-  $ [ ("fetcher", "github"), ("name", name), ("repo", repo) ]
-  ++ maybeToList ((,) "commit" <$> commit)
-  ++ maybeToList ((,) "branch" <$> branch)
--}
+fetchGitHub = Fetcher {..}
+  where
+    getRev name GitHub {..} tmp = getRev_Git name branch tmp
+    prefetch name GitHub {..} rev =
+      prefetchWith name "nix-prefetch-zip" args
+      where
+        args = [ "--url", T.unpack url, "--name", T.unpack filename ]
+        url = "https://github.com/" <> repo <> "/archive/" <> rev <> ".tar.gz"
+        filename = repo <> "-" <> rev <> "-src"
