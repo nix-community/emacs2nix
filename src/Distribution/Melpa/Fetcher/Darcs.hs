5323865b5d49b8b99d755e2b41338883492697b1
Thomas Tuegel <ttuegel@gmail.com>
2015-05-18 14:02:48 -0500

    Implement darcs fetcher

--- src/Distribution/Melpa/Fetcher/Darcs.hs
+++ src/Distribution/Melpa/Fetcher/Darcs.hs
@@ -1,14 +1,21 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module Distribution.Melpa.Fetcher.Darcs ( Darcs, fetchDarcs ) where
 
 import Control.Error
+import Control.Exception (bracket)
+import Control.Monad.IO.Class
 import Data.Aeson
 import Data.Aeson.Types (defaultOptions)
 import Data.Monoid ((<>))
 import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
 import GHC.Generics
+import qualified System.IO.Streams as S
+import System.IO.Temp (withSystemTempFile)
 
 import Distribution.Melpa.Fetcher
 
@@ -25,10 +32,22 @@ instance FromJSON Darcs where
   parseJSON = genericParseJSON defaultOptions
 
 fetchDarcs :: Fetcher Darcs
-fetchDarcs = undefined
+fetchDarcs = Fetcher {..}
+  where
+    getRev _ Darcs {..} tmp =
+      let args = [ "changes", "--context" ]
+      in liftIO $ bracket
+        (S.runInteractiveProcess "darcs" args (Just tmp) Nothing)
+        (\(_, _, _, pid) -> S.waitForProcess pid)
+        (\(inp, out, _, _) -> do
+               S.write Nothing inp
+               S.decodeUtf8 out >>= S.fold (<>) T.empty)
 
-{-
-hash :: FilePath -> FilePath -> Bool -> Text -> Archive -> Recipe
-     -> EitherT Text IO Package
-hash _ _ _ name _ _ = left (name <> ": fetcher 'darcs' not implemented")
--}
+    prefetch name Darcs {..} context =
+      EitherT $ withSystemTempFile "melpa2nix-darcs-context"
+        $ \contextFile handle -> do
+               -- darcs uses a long textual context instead of revision
+               -- numbers or hashes, so we need to put it in a file
+               T.hPutStr handle context
+               let args = [ T.unpack url, contextFile ]
+               runEitherT $ prefetchWith name "nix-prefetch-darcs" args
