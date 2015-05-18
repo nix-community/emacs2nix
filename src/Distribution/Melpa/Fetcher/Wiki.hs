3552f2caa232775034d09c4a84ddfe50905f636f
Thomas Tuegel <ttuegel@gmail.com>
2015-05-18 14:28:14 -0500

    Implement Wiki fetcher

--- src/Distribution/Melpa/Fetcher/Wiki.hs
+++ src/Distribution/Melpa/Fetcher/Wiki.hs
@@ -7,9 +7,9 @@ module Distribution.Melpa.Fetcher.Wiki ( Wiki, fetchWiki ) where
 import Control.Error hiding (runScript)
 import Data.Aeson
 import Data.Aeson.Types (defaultOptions)
-import Data.HashMap.Strict (HashMap)
-import qualified Data.HashMap.Strict as HM
+import Data.Monoid ((<>))
 import Data.Text (Text)
+import qualified Data.Text as T
 import GHC.Generics
 
 import Distribution.Melpa.Fetcher
@@ -27,24 +27,11 @@ instance FromJSON Wiki where
   parseJSON = genericParseJSON defaultOptions
 
 fetchWiki :: Fetcher Wiki
-fetchWiki = undefined
-
-{-
-hash :: FilePath -> FilePath -> Bool -> Text -> Archive -> Recipe
-     -> EitherT Text IO Package
-hash _ nixpkgs _ name arch rcp = do
-  let Wiki _wiki@(Fetcher {..}) = fetcher rcp
-  _hash <- prefetch nixpkgs name Nothing (wikiEnv name _wiki)
-  return Package
-    { Package.ver = Archive.ver arch
-    , Package.deps = maybe [] HM.keys (Archive.deps arch)
-    , Package.recipe = rcp
-    , Package.hash = _hash
-    }
-
-wikiEnv :: Text -> Wiki -> HashMap Text Text
-wikiEnv name Fetcher {..} =
-  HM.fromList
-  $ [ ("fetcher", "wiki"), ("name", name) ]
-  ++ maybeToList ((,) "url" <$> url)
--}
+fetchWiki = Fetcher {..}
+  where
+    getRev _ _ _ = return ""
+    prefetch name Wiki {..} _ =
+      prefetchWith name "nix-prefetch-url" [ T.unpack fullurl ]
+      where
+        fullurl = fromMaybe defaultUrl url
+        defaultUrl = "http://www.emacswiki.org/emacs/download/" <> name <> ".el"
