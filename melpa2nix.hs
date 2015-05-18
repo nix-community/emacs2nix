74da1048732222a059fae4151e7d4c1d20155454
Thomas Tuegel <ttuegel@gmail.com>
2015-05-18 15:02:34 -0500

    Don't define data type for command line options

--- melpa2nix.hs
+++ melpa2nix.hs
@@ -15,38 +15,33 @@ import qualified System.IO.Streams as S
 import Distribution.Melpa.Package
 import Distribution.Melpa.Recipe
 
-data Melpa2nix =
-  Melpa2nix
-  { packageBuild :: FilePath
-  , recipesDir :: FilePath
-  , recipesOut :: FilePath
-  , packagesOut :: FilePath
-  }
-
-melpa2nixParser :: Parser Melpa2nix
-melpa2nixParser =
-  Melpa2nix
-  <$> strOption (long "package-build" <> metavar "FILE" <> help "path to package-build.el")
-  <*> strOption (long "recipes-dir" <> metavar "DIR" <> help "path to MELPA recipes")
-  <*> strOption (long "recipes-out" <> metavar "FILE" <> help "dump MELPA recipes to FILE")
-  <*> strOption (long "packages-out" <> metavar "FILE" <> help "dump packages to FILE")
-
 main :: IO ()
-main = execParser opts >>= melpa2nix
-  where
-    opts = info
-           (helper <*> melpa2nixParser)
-           (fullDesc <> progDesc "Generate Nix expressions from MELPA recipes")
+main = execParser
+       (info (helper <*> melpa2nixParser)
+        (fullDesc <> progDesc "Generate Nix expressions from MELPA recipes"))
 
-melpa2nix :: Melpa2nix -> IO ()
-melpa2nix Melpa2nix {..} = do
+melpa2nixParser :: Parser (IO ())
+melpa2nixParser =
+  melpa2nix
+
+  <$> strOption (long "package-build" <> metavar "FILE"
+                 <> help "path to package-build.el")
+
+  <*> strOption (long "recipes-dir" <> metavar "DIR"
+                 <> help "path to MELPA recipes")
+
+  <*> strOption (long "recipes-out" <> metavar "FILE"
+                 <> help "dump MELPA recipes to FILE")
+
+  <*> strOption (long "packages-out" <> metavar "FILE"
+                 <> help "dump packages to FILE")
+
+melpa2nix :: FilePath -> FilePath -> FilePath -> FilePath -> IO ()
+melpa2nix packageBuild recipesDir recipesOut packagesOut = do
   dumpRecipes packageBuild recipesDir recipesOut
   recipes <- readRecipes packageBuild recipesOut
   epackages <- M.traverseWithKey (getPackage packageBuild recipesOut) recipes
-  for_ epackages $ \epkg ->
-    case epkg of
-      Left err -> T.putStrLn err
-      Right _ -> return ()
+  for_ epackages $ either T.putStrLn (\_ -> return ())
   let packages = M.map fromRight (M.filter isRight epackages)
   S.withFileAsOutput packagesOut $ \out -> do
     enc <- S.fromLazyByteString (encodePretty packages)
