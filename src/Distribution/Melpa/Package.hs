7b3277db5b30555faaa5c86099bc61f871fa916c
Thomas Tuegel <ttuegel@gmail.com>
2015-05-21 14:49:37 -0500

    Calculate dependencies again

--- src/Distribution/Melpa/Package.hs
+++ src/Distribution/Melpa/Package.hs
@@ -9,15 +9,17 @@ import Control.Error
 import Control.Exception (bracket)
 import Control.Monad.IO.Class
 import Data.Aeson
-import Data.Aeson.Types (parseMaybe)
+import Data.Aeson.Types (parseEither, parseMaybe)
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as M
 import Data.Monoid ((<>))
 import Data.Text (Text)
 import qualified Data.Text as T
+import System.Directory (doesFileExist, copyFile)
+import System.FilePath
 import qualified System.IO.Streams as S
 import qualified System.IO.Streams.Attoparsec as S
-import System.FilePath
+import System.IO.Temp (withSystemTempDirectory)
 
 import Distribution.Melpa.Fetcher
 import Distribution.Melpa.Recipe (Recipe(Recipe))
@@ -32,6 +34,7 @@ data Package =
   , recipe :: f
   , rev :: Text
   , hash :: Text
+  , deps :: [Text]
   }
 
 instance FromJSON Package where
@@ -40,6 +43,7 @@ instance FromJSON Package where
     rcp <- obj .: "recipe"
     rev <- obj .: "rev"
     hash <- obj .: "hash"
+    deps <- obj .: "deps"
     case rcp of
       Recipe { Recipe.recipe = recipe, Recipe.fetcher = fetcher } ->
         return Package {..}
@@ -51,6 +55,7 @@ instance ToJSON Package where
     , "recipe" .= Recipe { Recipe.fetcher = fetcher, Recipe.recipe = recipe }
     , "rev" .= rev
     , "hash" .= hash
+    , "deps" .= deps
     ]
 
 readPackages :: FilePath -> IO (Map Text Package)
@@ -75,7 +80,8 @@ getPackage packageBuildEl recipesEl workDir packages packageName rcp =
       case M.lookup packageName packages of
         Just pkg | rev pkg == rev_ -> return pkg
         _ -> do
-            (_, hash_) <- prefetch fetcher_ packageName recipe_ rev_
+            (path_, hash_) <- prefetch fetcher_ packageName recipe_ rev_
+            deps_ <- getDeps packageBuildEl recipesEl packageName path_
             return
                 Package
                 { ver = ver_
@@ -83,7 +89,35 @@ getPackage packageBuildEl recipesEl workDir packages packageName rcp =
                 , recipe = recipe_
                 , fetcher = fetcher_
                 , hash = hash_
+                , deps = M.keys deps_
                 }
+
+getDeps :: FilePath -> FilePath -> Text -> FilePath
+        -> EitherT Text IO (Map Text [Integer])
+getDeps packageBuildEl recipesEl packageName sourceDirOrEl = do
+  getDepsEl <- liftIO $ getDataFileName "checkout.el"
+  isEl <- liftIO $ doesFileExist sourceDirOrEl
+  let withSourceDir act
+        | isEl = do
+            let tmpl = "melpa2nix-" <> T.unpack packageName
+                elFile = T.unpack packageName <.> "el"
+            withSystemTempDirectory tmpl $ \sourceDir -> do
+              copyFile sourceDirOrEl (sourceDir </> elFile)
+              act sourceDir
+        | otherwise = act sourceDirOrEl
+  handleAll $ EitherT $ withSourceDir $ \sourceDir -> do
+    let args = [ "--batch"
+               , "-l", packageBuildEl
+               , "-l", getDepsEl
+               , "-f", "get-deps", recipesEl, T.unpack packageName, sourceDir
+               ]
+    bracket
+      (S.runInteractiveProcess "emacs" args Nothing Nothing)
+      (\(_, _, _, pid) -> S.waitForProcess pid)
+      (\(inp, out, _, _) -> do
+             S.write Nothing inp
+             result <- parseEither parseJSON <$> S.parseFromStream json' out
+             return (either (Left . T.pack) Right result))
 
 getVersion :: FilePath -> FilePath -> Text -> FilePath -> EitherT Text IO Text
 getVersion packageBuildEl recipesEl packageName sourceDir = do
