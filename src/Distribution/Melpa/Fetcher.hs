557015fb6f4eced541500219f85e0caaa74c9df9
Thomas Tuegel <ttuegel@gmail.com>
2015-05-21 13:34:46 -0500

    Assume well-behaved prefetch scripts

--- src/Distribution/Melpa/Fetcher.hs
+++ src/Distribution/Melpa/Fetcher.hs
@@ -11,6 +11,7 @@ import Data.Monoid ((<>))
 import Data.Text (Text)
 import qualified Data.Text as T
 import GHC.Generics
+import System.Environment (getEnvironment)
 import System.Exit (ExitCode(..))
 import qualified System.IO.Streams as S
 
@@ -29,23 +30,24 @@ wrapFetcher fetch val =
 
 prefetchWith :: Text -> FilePath -> [String] -> EitherT Text IO (FilePath, Text)
 prefetchWith _ prefetcher args = handleAll $ EitherT $ do
+  oldEnv <- HM.fromList <$> getEnvironment
+  let env = HM.toList (HM.insert "PRINT_PATH" "1" oldEnv)
   bracket
-    (S.runInteractiveProcess prefetcher args Nothing Nothing)
+    (S.runInteractiveProcess prefetcher args Nothing (Just env))
     (\(_, _, _, pid) -> S.waitForProcess pid)
-    (\(inp, _, out, pid) -> do
+    (\(inp, out, _, pid) -> do
            S.write Nothing inp
-           lines_ <- S.lines out >>= S.decodeUtf8 >>= S.toList
-           let hashes = mapMaybe (T.stripPrefix "hash is ") lines_
-               paths = mapMaybe (T.stripPrefix "path is ") lines_
-               anyerr = "; output was:\n" <> T.unlines lines_
+           _lines <- S.lines out >>= S.decodeUtf8 >>= S.toList
+           let anyerr = "; output was:\n" <> T.unlines _lines
            exitCode <- S.waitForProcess pid
            case exitCode of
              ExitFailure errno ->
                return
                $ Left ("prefetcher failed with exit code " <> T.pack (show errno))
              ExitSuccess -> return $ do
-               hash_ <- headErr ("could not fild hash" <> anyerr) hashes
-               path_ <- headErr ("could not fild path" <> anyerr) paths
+               hash_ <- headErr ("could not find hash" <> anyerr) _lines
+               _lines <- return (tail _lines)
+               path_ <- headErr ("could not find path" <> anyerr) _lines
                return (T.unpack path_, hash_))
 
 handleAll :: EitherT Text IO a -> EitherT Text IO a
