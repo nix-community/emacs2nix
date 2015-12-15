361148bdc72efacef2e7320b0f5e1e41325eaae3
Thomas Tuegel <ttuegel@gmail.com>
2015-12-15 17:09:37 -0600

    Melpa: remove extra exception types

--- src/Distribution/Melpa.hs
+++ src/Distribution/Melpa.hs
@@ -51,11 +51,6 @@ readMelpa packagesJson =
          Left parseError -> throwIO (ParseMelpaError parseError)
          Right packages -> pure packages)
 
-data ArchiveError = ArchiveError SomeException
-  deriving (Show, Typeable)
-
-instance Exception ArchiveError
-
 getMelpa :: Int
          -> FilePath
          -> Bool
@@ -64,28 +59,27 @@ getMelpa :: Int
          -> Set Text
          -> IO (Map Text Nix.Package)
 getMelpa nthreads melpaDir stable workDir oldPackages packages
-  = mapExceptionIO ArchiveError getMelpa_go
+  = do
+    melpaCommit <- revision_Git Nothing melpaDir
+
+    recipes <- readRecipes melpaDir
+
+    createDirectoryIfMissing True workDir
+
+    sem <- (if nthreads > 0 then pure nthreads else getNumCapabilities) >>= newQSem
+    let getPackage_ name recipe
+          | Set.null packages || Set.member name packages
+            = getPackage sem melpaDir melpaCommit stable workDir name recipe
+          | otherwise
+            = return Nothing
+        getPackages = M.traverseWithKey getPackage_ recipes
+    newPackages <- catMaybesMap <$> runConcurrently getPackages
+
+    (pure . Nix.cleanNames) (newPackages <> oldPackages)
+
   where
     catMaybesMap = M.fromList . mapMaybe liftMaybe . M.toList
     liftMaybe (x, y) = (,) x <$> y
-
-    getMelpa_go = do
-      melpaCommit <- revision_Git Nothing melpaDir
-
-      recipes <- readRecipes melpaDir
-
-      createDirectoryIfMissing True workDir
-
-      sem <- (if nthreads > 0 then pure nthreads else getNumCapabilities) >>= newQSem
-      let getPackage_ name recipe
-            | Set.null packages || Set.member name packages
-              = getPackage sem melpaDir melpaCommit stable workDir name recipe
-            | otherwise
-              = return Nothing
-          getPackages = M.traverseWithKey getPackage_ recipes
-      newPackages <- catMaybesMap <$> runConcurrently getPackages
-
-      (pure . Nix.cleanNames) (newPackages <> oldPackages)
 
 concurrentlyLimited :: QSem -> IO a -> Concurrently a
 concurrentlyLimited sem go
@@ -203,36 +197,26 @@ getFetcher _ _ Darcs {..} = throwIO DarcsFetcherNotImplemented
 
 getFetcher _ _ Fossil {..} = throwIO FossilFetcherNotImplemented
 
-data VersionError = VersionError SomeException
+data NoVersion = NoVersion
   deriving (Show, Typeable)
 
-instance Exception VersionError
-
-data NoVersionAvailable = NoVersionAvailable
-  deriving (Show, Typeable)
-
-instance Exception NoVersionAvailable
+instance Exception NoVersion
 
 getVersion :: FilePath -> Bool -> FilePath -> Text -> FilePath -> IO Text
 getVersion packageBuildEl stable recipeFile packageName sourceDir
-  = mapExceptionIO VersionError getVersion_go
-  where
-    getVersion_go = do
-      checkoutEl <- getDataFileName "checkout.el"
-      let args = [ "--batch"
-                 , "-l", packageBuildEl
-                 , "-l", checkoutEl
-                 , "-f", if stable then "checkout-stable" else "checkout"
-                 , recipeFile, T.unpack packageName, sourceDir
-                 ]
-      runInteractiveProcess "emacs" args Nothing Nothing $ \out -> do
-        result <- liftIO (S.fold (<>) Nothing =<< S.map Just =<< S.decodeUtf8 out)
-        case result of
-          Nothing -> throwIO NoVersionAvailable
-          Just ver -> pure ver
-
-data DepsError = DepsError SomeException
-  deriving (Show, Typeable)
+  = do
+    checkoutEl <- getDataFileName "checkout.el"
+    let args = [ "--batch"
+                , "-l", packageBuildEl
+                , "-l", checkoutEl
+                , "-f", if stable then "checkout-stable" else "checkout"
+                , recipeFile, T.unpack packageName, sourceDir
+                ]
+    runInteractiveProcess "emacs" args Nothing Nothing $ \out -> do
+      result <- liftIO (S.fold (<>) Nothing =<< S.map Just =<< S.decodeUtf8 out)
+      case result of
+        Nothing -> throwIO NoVersion
+        Just ver -> pure ver
 
 instance Exception DepsError
 
@@ -243,58 +227,51 @@ instance Exception ParseDepsError
 
 getDeps :: FilePath -> FilePath -> Text -> FilePath -> IO (Map Text [Integer])
 getDeps packageBuildEl recipeFile packageName sourceDirOrEl
-  = mapExceptionIO DepsError getDeps_go
-  where
-    getDeps_go = do
-      getDepsEl <- getDataFileName "get-deps.el"
-      isEl <- doesFileExist sourceDirOrEl
-      let withSourceDir act
-            | isEl = do
-                let tmpl = "melpa2nix-" <> T.unpack packageName
-                    elFile = T.unpack packageName <.> "el"
-                withSystemTempDirectory tmpl $ \sourceDir -> do
-                  copyFile sourceDirOrEl (sourceDir </> elFile)
-                  act sourceDir
-            | otherwise = act sourceDirOrEl
-      withSourceDir $ \sourceDir -> do
-        let args = [ "--batch"
-                   , "-l", packageBuildEl
-                   , "-l", getDepsEl
-                   , "-f", "get-deps", recipeFile, T.unpack packageName, sourceDir
-                   ]
-        runInteractiveProcess "emacs" args Nothing Nothing $ \out -> do
-          result <- liftIO (parseEither parseJSON <$> S.parseFromStream json' out)
-          case result of
-            Left err -> throwIO (ParseDepsError err)
-            Right deps_ -> pure deps_
+  = do
+    getDepsEl <- getDataFileName "get-deps.el"
+    isEl <- doesFileExist sourceDirOrEl
+    let withSourceDir act
+          | isEl = do
+              let tmpl = "melpa2nix-" <> T.unpack packageName
+                  elFile = T.unpack packageName <.> "el"
+              withSystemTempDirectory tmpl $ \sourceDir -> do
+                copyFile sourceDirOrEl (sourceDir </> elFile)
+                act sourceDir
+          | otherwise = act sourceDirOrEl
+    withSourceDir $ \sourceDir -> do
+      let args = [ "--batch"
+                  , "-l", packageBuildEl
+                  , "-l", getDepsEl
+                  , "-f", "get-deps", recipeFile, T.unpack packageName, sourceDir
+                  ]
+      runInteractiveProcess "emacs" args Nothing Nothing $ \out -> do
+        result <- liftIO (parseEither parseJSON <$> S.parseFromStream json' out)
+        case result of
+          Left err -> throwIO (ParseDepsError err)
+          Right deps_ -> pure deps_
 
-data RevisionError = RevisionError SomeException
+data NoRevision = NoRevision
   deriving (Show, Typeable)
 
-instance Exception RevisionError
-
-data NoRevisionError = NoRevisionError
-  deriving (Show, Typeable)
-
-instance Exception NoRevisionError
+instance Exception NoRevision
 
 revision_Bzr :: FilePath -> IO Text
-revision_Bzr tmp = mapExceptionIO RevisionError $ do
+revision_Bzr tmp = do
   let args = [ "log", "-l1", tmp ]
   runInteractiveProcess "bzr" args Nothing Nothing $ \out -> do
     let getRevno = (T.strip <$>) . T.stripPrefix "revno:"
     revnos <- mapMaybe getRevno <$> liftIO (S.lines out >>= S.decodeUtf8 >>= S.toList)
     case revnos of
       (rev:_) -> pure rev
-      _ -> throwIO NoRevisionError
+      _ -> throwIO NoRevision
 
 revision_Git :: Maybe Text -> FilePath -> IO Text
-revision_Git branch tmp = mapExceptionIO RevisionError $ do
+revision_Git branch tmp = do
   runInteractiveProcess "git" gitArgs (Just tmp) Nothing $ \out -> do
     revs <- liftIO (S.lines out >>= S.decodeUtf8 >>= S.toList)
     case revs of
       (rev:_) -> pure rev
-      _ -> throwIO NoRevisionError
+      _ -> throwIO NoRevision
   where
     fullBranch = do
         branch_ <- branch
@@ -305,13 +282,13 @@ revision_Git branch tmp = mapExceptionIO RevisionError $ do
               ++ maybeToList fullBranch
 
 revision_Hg :: FilePath -> IO Text
-revision_Hg tmp = mapExceptionIO RevisionError $ do
+revision_Hg tmp = do
   runInteractiveProcess "hg" ["tags"] (Just tmp) Nothing $ \out -> do
     lines_ <- liftIO (S.lines out >>= S.decodeUtf8 >>= S.toList)
     let revs = catMaybes (hgRev <$> lines_)
     case revs of
       (rev:_) -> pure rev
-      _ -> throwIO NoRevisionError
+      _ -> throwIO NoRevision
   where
     hgRev txt = do
         afterTip <- T.strip <$> T.stripPrefix "tip" txt
@@ -321,12 +298,12 @@ revision_Hg tmp = mapExceptionIO RevisionError $ do
           else return rev
 
 revision_SVN :: FilePath -> IO Text
-revision_SVN tmp = mapExceptionIO RevisionError $ do
+revision_SVN tmp = do
   runInteractiveProcess "svn" ["info"] (Just tmp) Nothing $ \out -> do
     lines_ <- liftIO (S.lines out >>= S.decodeUtf8 >>= S.toList)
     let revs = catMaybes (svnRev <$> lines_)
     case revs of
       (rev:_) -> pure rev
-      _ -> throwIO NoRevisionError
+      _ -> throwIO NoRevision
   where
     svnRev = fmap T.strip . T.stripPrefix "Revision:"
