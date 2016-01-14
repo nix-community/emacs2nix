3248b5ef04cba61e838ddc6427e0c24e3e7d0eb5
Thomas Tuegel <ttuegel@gmail.com>
2016-01-14 14:56:00 -0600

    add wl-pprint-text dependency

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { mkDerivation, aeson, aeson-pretty, async, attoparsec, base
 , bytestring, containers, directory, errors, filepath, io-streams
 , optparse-applicative, stdenv, temporary, text, text-regex-replace
-, transformers, unordered-containers
+, transformers, unordered-containers, wl-pprint-text
 }:
 mkDerivation {
   pname = "emacs2nix";
@@ -12,12 +12,12 @@ mkDerivation {
   libraryHaskellDepends = [
     aeson async attoparsec base bytestring containers directory errors
     filepath io-streams temporary text text-regex-replace transformers
-    unordered-containers
+    unordered-containers wl-pprint-text
   ];
   executableHaskellDepends = [
     aeson aeson-pretty async base bytestring containers directory
     errors filepath io-streams optparse-applicative temporary text
-    unordered-containers
+    transformers unordered-containers wl-pprint-text
   ];
   description = "Automatically generate Nix expressions for Emacs packages";
   license = stdenv.lib.licenses.gpl3;
