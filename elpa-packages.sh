438b4281bd39b74706a7694417d78c47d0a2361a
Thomas Tuegel <ttuegel@gmail.com>
2016-04-19 08:29:05 -0500

    elpa-packages.sh: inherit SSL_CERT_FILE if set

--- elpa-packages.sh
+++ elpa-packages.sh
@@ -3,4 +3,7 @@
 
 # usage: ./elpa-packages.sh -o PATH
 
+## env var for curl
+export SSL_CERT_FILE=${SSL_CERT_FILE:+/etc/ssl/certs/ca-certificates.crt}
+
 cabal run elpa2nix -- https://elpa.gnu.org/packages/ "$@"
