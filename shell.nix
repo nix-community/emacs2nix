9d2777a5b633ca03bb5e6d83d1dffd4b0a6ccd93
Thomas Tuegel <ttuegel@gmail.com>
2016-04-26 14:18:36 -0500

    bundle Nixpkgs

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
-{ nixpkgs ? import <nixpkgs> {}, profiling ? false }:
+{ nixpkgs ? import ./nixpkgs {}, profiling ? false }:
 
 with nixpkgs;
 
@@ -11,6 +11,7 @@ let
         enableLibraryProfiling = profiling;
       });
 
+      aeson = self.aeson_0_11_1_4;
       comonad = lib.doJailbreak (lib.dontCheck super.comonad);
       distributive = lib.dontCheck super.distributive;
       fail = lib.dontHaddock super.fail;
@@ -24,11 +25,17 @@ let
       unordered-containers = lib.doJailbreak super.unordered-containers;
     };
   };
+  omitDirs = [ ".git" "dist" "nixpkgs" "hnix" ];
+  filterCabalSource = drv: pred:
+    lib.overrideCabal drv (args: args // { src = builtins.filterSource pred args.src; });
 in
 lib.addBuildTools
-  (haskellPackages.callPackage ./. {})
-  [
-    emacs cabal-install
-    nix nix-prefetch-scripts
-    bazaar cvs curl darcs fossil git mercurial subversion
-  ]
+  (filterCabalSource
+   (haskellPackages.callPackage ./. {})
+   (path: type:
+        type != "directory" || !(lib.elem (baseNameOf path) omitDirs)))
+[
+  emacs cabal-install
+  nix nix-prefetch-scripts
+  bazaar cvs curl darcs fossil git mercurial subversion
+]
