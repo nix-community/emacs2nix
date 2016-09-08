c539017ad7f99f776d047c4e109b53184499d86a
Thomas Tuegel <ttuegel@gmail.com>
2016-09-08 07:36:59 -0500

    Take advantage of binary caching
    Fixes #32. The nixpkgs submodule is updated to take advantage of the
    binary cache from the upcoming NixOS 16.09 channel.

--- shell.nix
+++ shell.nix
@@ -4,41 +4,39 @@ with nixpkgs;
 
 let
   inherit (pkgs.haskell) lib;
-  haskellPackages = pkgs.haskell.packages.ghc7103.override {
+  haskellPackages = pkgs.haskell.packages.ghc801.override {
     overrides = self: super: {
 
       mkDerivation = args: super.mkDerivation (args // {
         enableLibraryProfiling = profiling;
       });
 
-      comonad = lib.doJailbreak (lib.dontCheck super.comonad);
-      distributive = lib.dontCheck super.distributive;
-      fail = lib.dontHaddock super.fail;
       hnix = self.callPackage ./hnix {};
-      parsers = lib.doJailbreak super.parsers;
-      reducers = lib.doJailbreak super.reducers;
-      semigroupoids = lib.dontCheck super.semigroupoids;
-      trifecta = lib.doJailbreak (lib.dontCheck super.trifecta);
-      unordered-containers = lib.doJailbreak super.unordered-containers;
     };
   };
-  omitDirs = [ ".git" "dist" "nixpkgs" "hnix" ];
-  filterCabalSource = pred: drv:
-    lib.overrideCabal drv (args: args // { src = builtins.filterSource pred args.src; });
+  filterSource = drv: pred:
+    lib.overrideCabal drv
+    (args: args // { src = builtins.filterSource pred args.src; });
+  omitDirs =
+    let
+      omitted = [ ".git" "dist" "nixpkgs" "hnix" ];
+    in drv:
+    filterSource drv
+    (path: type:
+      type != "directory" || !(stdenv.lib.elem (baseNameOf path) omitted));
   addCertPath = drv:
-    lib.overrideCabal drv (args: args // {
+    lib.overrideCabal drv
+    (args: args // {
       shellHook = ''
         export SSL_CERT_FILE="${cacert}/etc/ssl/certs/ca-bundle.crt"
       '';
     });
+  addBuildTools = drv:
+    lib.addBuildTools drv
+    [
+      emacs cabal-install
+      nix nix-prefetch-scripts
+      bazaar cvs curl darcs fossil git mercurial subversion
+    ];
 in
-lib.addBuildTools
-  (filterCabalSource
-    (path: type:
-        type != "directory" || !(stdenv.lib.elem (baseNameOf path) omitDirs))
-    (addCertPath (haskellPackages.callPackage ./. {})))
-[
-  emacs cabal-install
-  nix nix-prefetch-scripts
-  bazaar cvs curl darcs fossil git mercurial subversion
-]
+addBuildTools (omitDirs (addCertPath (haskellPackages.callPackage ./. {})))
